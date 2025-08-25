0bf777ed27e29fa4df4e91b4e2dcaf2bb58dfc74
UthiraMuthu <uthiramuthusp29@gmail.com>
2025-08-26 00:26:58 +0530

    API key

--- netlify/functions/proxy.jsx
+++ netlify/functions/proxy.jsx
@@ -8,7 +8,7 @@ export async function handler(event) {
       `http://64.227.189.27/wallet/api/v1/transaction_history?service_id=${service_id}&page=${page}`,
       {
         headers: {
-          Authorization: `Bearer ${process.env.API_TOKEN}`, // put token in Netlify env var
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMjgyMDUiLCJtb2JpbGUiOiI3MzU4MjIxMzU0IiwiYXBwX2lkIjoiNjAiLCJtaWQiOiIzNDgiLCJ0b2tlbiI6IjZjZjFhMzNhZDJkOGQyNjFkMWYwNDBiMWIwZGViMjc1IiwiZ3JvdXBJZCI6IjIxMDYxIiwiaXNzIjoiMjgyMDUifQ.ADopz72M1Z-eKpFXJd04RZvLxXHyJ8fFaT4HnzxxQCk`, // put token in Netlify env var
         },
       }
     );
