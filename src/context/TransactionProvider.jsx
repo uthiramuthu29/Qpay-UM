88d6237041a572bf122af6e572b5fa06219b63ef
UthiraMuthu <uthiramuthusp29@gmail.com>
2025-08-25 09:18:38 +0530

    API issue

--- src/context/TransactionProvider.jsx
+++ src/context/TransactionProvider.jsx
@@ -9,12 +9,9 @@ export const TransactionProvider = ({ children }) => {
 
   const fetchTransactions = async (pageNo = 0) => {
     setLoading(true);
-    console.log("Fetching from:", `/api/wallet/api/v1/transaction_history?service_id=111&page=${pageNo}`);
-    try {
-        
-
+    try {        
       const response = await fetch(
-        `/api/wallet/api/v1/transaction_history?service_id=111&page=${pageNo}`,
+        `https://64.227.189.27/wallet/api/v1/transaction_history?service_id=111&page=${pageNo}`,
         {
           headers: {
             Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMjgyMDUiLCJtb2JpbGUiOiI3MzU4MjIxMzU0IiwiYXBwX2lkIjoiNjAiLCJtaWQiOiIzNDgiLCJ0b2tlbiI6IjZjZjFhMzNhZDJkOGQyNjFkMWYwNDBiMWIwZGViMjc1IiwiZ3JvdXBJZCI6IjIxMDYxIiwiaXNzIjoiMjgyMDUifQ.ADopz72M1Z-eKpFXJd04RZvLxXHyJ8fFaT4HnzxxQCk`,
